a514008c4e6c6f633efc98cddfd4bfb4b7db2ba9
charles <chavanoise@free.fr>
2026-06-23 16:24:36 +0200

    v1.0 - pour test

--- maintain.inc.php
+++ maintain.inc.php
@@ -5,25 +5,24 @@ class private_derivative_protection_maintain extends PluginMaintain
 {
   private $htaccess_content = "# private_derivative_protection\nRequire all denied\n";
 
-  function activate($plugin_version, &$errors = array())
-  {
-    // Protège les répertoires d'originaux contre l'accès HTTP direct.
-    // PHP peut toujours lire les fichiers depuis le filesystem (serve_original.php).
-    file_put_contents(PHPWG_ROOT_PATH . 'galleries/.htaccess', $this->htaccess_content);
-    file_put_contents(PHPWG_ROOT_PATH . 'upload/.htaccess',   $this->htaccess_content);
-  }
+function activate($plugin_version, &$errors = array())
+{
+  conf_update_param('original_url_protection', 'all');
+  file_put_contents(PHPWG_ROOT_PATH . 'galleries/.htaccess', $this->htaccess_content);
+  file_put_contents(PHPWG_ROOT_PATH . 'upload/.htaccess',   $this->htaccess_content);
+}
 
-  function deactivate()
-  {
-    // Retire la protection pour ne pas bloquer les autres plugins
-    // qui construisent des URLs directes vers les originaux.
-    @unlink(PHPWG_ROOT_PATH . 'galleries/.htaccess');
-    @unlink(PHPWG_ROOT_PATH . 'upload/.htaccess');
-  }
+function deactivate()
+{
+  conf_update_param('original_url_protection', '');
+  @unlink(PHPWG_ROOT_PATH . 'galleries/.htaccess');
+  @unlink(PHPWG_ROOT_PATH . 'upload/.htaccess');
+}
 
-  function uninstall()
-  {
-    @unlink(PHPWG_ROOT_PATH . 'galleries/.htaccess');
-    @unlink(PHPWG_ROOT_PATH . 'upload/.htaccess');
-  }
+function uninstall()
+{
+  conf_update_param('original_url_protection', '');
+  @unlink(PHPWG_ROOT_PATH . 'galleries/.htaccess');
+  @unlink(PHPWG_ROOT_PATH . 'upload/.htaccess');
+}
 }
